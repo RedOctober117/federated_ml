7a4a0f4eb7785559a944a95d13f350dad530e01d
RedOctober117 <dbcahilly3@gmail.com>
2024-07-12 12:49:52 -0500

    updates

--- src/client.py
+++ src/client.py
@@ -5,7 +5,7 @@ import time
 from matplotlib.pylab import normal
 import sklearn
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error, median_absolute_error
-import sklearn.metrics
+from sklearn.metrics import r2_score
 import tensorflow as tf
 import keras
 import pandas as pd
@@ -15,6 +15,8 @@ from sklearn.preprocessing import MinMaxScaler
 from flwr.common.logger import log
 from pathlib import Path
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.seasonal import seasonal_decompose
+import yfinance as yf
 
 time_ = int(time.time())
 
@@ -29,6 +31,24 @@ data_df['Start Date'] = pd.to_datetime(data_df['Start Date'])
 data_df['Start Date'] = data_df['Start Date'].dt.floor('D')
 data_df.set_index('Start Date', inplace=True)
 
+# tf_data = pd.read_csv('all_sample.csv')
+# tf_retained_columns = ['datetime', 'I5-N VDS 759576', 'I5-N VDS 763237', 'I5-N VDS 759602', 'I5-N VDS 716974', 'I5-S VDS 71693']
+# tf_data_df = tf_data.loc[:, tf_retained_columns]
+
+# tf_data_df = yf.download("SPY", start="2017-01-01", end="2018-01-01")
+# # print(tf_data_df.index)
+# # plt.plot(tf_data_df['Close'])
+# # plt.show(block=False)
+# # plt.clf()
+# tf_normalized_df = pd.DataFrame()
+# tf_normalized_df.index = tf_data_df.index
+# tf_scalar = MinMaxScaler(feature_range=(0,1))
+# # tf_normalized_df['datetime'] = pd.to_datetime(tf_data_df['Date'], format='%m/%d/%Y %H:%M')
+# tf_normalized_df['Close'] = tf_scalar.fit_transform(tf_data_df['Close'].to_numpy().reshape(-1, 1))
+
+# tf_normalized_df = tf_normalized_df.pop('Close')
+# tf_test_df_1 = tf_normalized_df[:int(len(tf_normalized_df) * .7)]
+# tf_test_df_2 = tf_normalized_df[int(len(tf_normalized_df) * .7):]
 # stations = {}
 # for key in data_df['Station Name']:
 #   if key in stations:
@@ -65,6 +85,7 @@ client_tables = {
   ]
 }
 
+
 for key, clients in client_tables.items():
   temp_list = []
   for client in clients:
@@ -95,8 +116,19 @@ for cluster in merged_clients:
     normalized_clients[client] = scalar.fit_transform(cluster[client].to_numpy().reshape(-1, 1))
   normalized_clusters.append(normalized_clients)
 
+unobserved_test = data_df[data_df['Station Name'] == 'PALO ALTO CA / TED THOMPSON #1'].pop('Energy (kWh)')
+fitted_unobserved_test = unobserved_test.resample('D', group_keys=True).sum()
+fitted_unobserved_test = pd.DataFrame(ExponentialSmoothing(fitted_unobserved_test, trend='add', seasonal='add', seasonal_periods=30).fit().fittedvalues)['2018-01-01':'2020-01-01']
+normalized_unobserved_test = pd.DataFrame()
+normalized_unobserved_test.index = fitted_unobserved_test.index
+normalized_unobserved_test[0] = scalar.fit_transform(fitted_unobserved_test.to_numpy().reshape(-1, 1))
+normalized_unobserved_test_x = normalized_unobserved_test[:'2019-01-01']
+normalized_unobserved_test_y = normalized_unobserved_test['2019-01-01':]
+
+
 # for cluster in normalized_clusters:
-#   print(cluster.info(), end='\n\n\n')
+#   # print(cluster.)
+#   print(cluster.describe(), end='\n\n\n')
 
 training_clusters = list()
 testing_clusters = list()
@@ -119,20 +151,36 @@ def split_sequence(sequence, n_steps):
     y.append(seq_y)
   return np.array(X), np.array(y)
 
+def split_dataframe(df: pd.DataFrame, offset) -> tuple[pd.DataFrame, pd.DataFrame]:
+  x_df = df.copy(deep=True)
+  x_df = x_df.iloc[:len(df) - 1]
+
+  y_df = df.shift(periods=offset)
+  y_df = y_df.iloc[offset:]
+  # y_df = y_df.truncate(after=len(x_df) - 6, axis=0, copy=False)
+  return x_df, y_df
+
 steps = 1
 
 prepared_clusters = list()
-for cluster_index in range(len(normalized_clusters)):
+for cluster_index in range(len(training_clusters)):
   prepared_cluster = list()
-  for client in normalized_clusters[cluster_index]:
-    prepared_cluster.append((*split_sequence(normalized_clusters[cluster_index][client], steps), testing_clusters[cluster_index][client]))
+  for client in training_clusters[cluster_index]:
+    print(client)
+    prepared_cluster.append((*split_sequence(training_clusters[cluster_index][client], steps), *split_sequence(testing_clusters[cluster_index][client], steps), normalized_clusters[cluster_index][client]))
   prepared_clusters.append(prepared_cluster)
 
+# print(split_sequence(training_clusters[0]['01'], steps)[2])
+# print(prepared_clusters[0][0][2].to_numpy())
+# exit()
+
 class Client():
-  def __init__(self, x_train, y_train, test_df):
-    self._x_train = x_train
-    self._y_train = y_train
-    self.test_df = test_df
+  def __init__(self, x_train, y_train, x_test, y_test, full_df):
+    self.x_train = x_train
+    self.y_train = y_train
+    self.x_test = x_test
+    self.y_test = y_test
+    self.full_df = full_df
 
     self.model = keras.Sequential()
     self.model.add(keras.layers.InputLayer((steps, 1)))
@@ -147,26 +195,43 @@ class Client():
       for layer_index in range(len(self.model.layers)):
         self.model.layers[layer_index].set_weights(weights[layer_index])
 
-    self.model.fit(self._x_train, self._y_train, epochs=epochs, shuffle=False)
+    self.model.fit(self.x_train, self.y_train, validation_data=(self.x_test, self.y_test), epochs=epochs, shuffle=False)
   
   def evaluate(self, label, path):
-    yhat = self.model.predict(self.test_df)
+    # yhat = pd.DataFrame(self.model.predict(self.x_train), index=self.test_df.index)
+    yhat = self.model.predict(self.x_test)
+    print(len(yhat))
+
     plt.xlabel('events')
     plt.ylabel('traffic')
     plt.title(f'Model {label}')
-    plt.plot(self.test_df, label='true')
-    plt.plot(pd.DataFrame(yhat, index=self.test_df.index), label='predicted')
+    plt.plot(self.y_test, label='true')
+    plt.plot(yhat, label='predicted')
     plt.legend()
     plt.savefig(f'{path}/model_{label}')
     plt.clf()
+ 
+def plot_evaluation(yhat, actual, title, path, invert=True):
+  if invert:
+    yhat = scalar.inverse_transform(yhat)
+    actual = pd.DataFrame(scalar.inverse_transform(actual.reshape(-1, 1)))
 
+  plt.xlabel('events')
+  plt.ylabel('Energy (kWh)')
+  plt.title(f'{title}')
+  plt.plot(actual, label='actual')
+  plt.plot(yhat, label='predicted')
+  plt.legend()
+  plt.savefig(f'{path}')
+  plt.clf()
 
+  return yhat, actual
 
 cluster_client_models = list()
 for cluster in prepared_clusters:
   cluster_clients = list()
   for client in cluster:
-    cluster_clients.append(Client(client[0], client[1], client[2]))
+    cluster_clients.append(Client(client[0], client[1], client[2], client[3], client[4]))
   cluster_client_models.append(cluster_clients)
 
 def federated_learning(clients, test_df, rounds=3, epochs=100) -> keras.models.Sequential:
@@ -208,8 +273,8 @@ def federated_learning(clients, test_df, rounds=3, epochs=100) -> keras.models.S
 
   return global_model
 
-round_count = 5
-epoch_count = 20
+round_count = 3
+epoch_count = 200
 model_layout = """
 Client Model:
 self.model = keras.Sequential()
@@ -244,28 +309,29 @@ for cluster in cluster_client_models:
   logs.append(f'\n\t### CLUSTER {j} ###\n')
 
   for client in cluster:
-    global_test = client.test_df
-    yhat = model.predict(global_test)
-    yhat = scalar.inverse_transform(yhat)
-    global_test = scalar.inverse_transform(global_test.to_numpy().reshape(-1, 1))
+    yhat, actual = plot_evaluation(model.predict(client.x_train), client.x_test, f'Global Model Observed Prediction {time_}', f'{path.as_posix()}/cluster_{j}/global_model_local_{i}_{int(time_)}.png')
+    # global_test = client.x_test
+    # yhat = model.predict(global_test)
+    # yhat = scalar.inverse_transform(yhat)
+    # global_test = scalar.inverse_transform(global_test)
 
-    plt.xlabel('events')
-    plt.ylabel('Energy (kWh)')
-    plt.title(f'Global Model Observed Prediction {time_}')
-    plt.plot(global_test, label='true')
-    plt.plot(yhat, label='predicted')
-    plt.legend()
-    plt.savefig(f'{path.as_posix()}/cluster_{j}/global_model_local_{i}_{int(time_)}.png')
-    plt.clf()
+    # plt.xlabel('events')
+    # plt.ylabel('Energy (kWh)')
+    # plt.title(f'Global Model Observed Prediction {time_}')
+    # plt.plot(scalar.inverse_transform(client.y_test.reshape(-1, 1)), label='true')
+    # plt.plot(yhat, label='predicted')
+    # plt.legend()
+    # plt.savefig(f'{path.as_posix()}/cluster_{j}/global_model_local_{i}_{int(time_)}.png')
+    # plt.clf()
 
     logs.append(f'\t\n\t### CLIENT {i} ###\n')
     logs.append(f'\tGlobal Model: Rounds: {round_count} Epochs: {epoch_count}  Steps: {steps}\n')
-    logs.append(f'\tLSTM R2 score {sklearn.metrics.r2_score(global_test, yhat)}\n')
-    logs.append(f'\tLSTM MSE score {mean_squared_error(global_test, yhat)}\n')
-    logs.append(f'\tLSTM MAPE score {mean_absolute_percentage_error(global_test, yhat)}\n')
-    logs.append(f'\tLSTM MAE score {mean_absolute_error(global_test, yhat)}\n')
-    logs.append(f'\tLSTM MDAE score {median_absolute_error(global_test, yhat)}\n')
-    logs.append(f'\tLSTM RMSE score {math.sqrt(mean_squared_error(global_test, yhat))}\n')
+    logs.append(f'\tLSTM R2 score {r2_score(actual, yhat)}\n')
+    logs.append(f'\tLSTM MSE score {mean_squared_error(actual, yhat)}\n')
+    logs.append(f'\tLSTM MAPE score {mean_absolute_percentage_error(actual, yhat)}\n')
+    logs.append(f'\tLSTM MAE score {mean_absolute_error(actual, yhat)}\n')
+    logs.append(f'\tLSTM MDAE score {median_absolute_error(actual, yhat)}\n')
+    logs.append(f'\tLSTM RMSE score {math.sqrt(mean_squared_error(actual, yhat))}\n')
     i += 1
 
   with open(f'{path.as_posix()}/cluster_{j}/log.txt', 'w') as file:
@@ -295,19 +361,25 @@ for layer_index in range(len(meta_model.layers)):
 
   meta_model.layers[layer_index].set_weights(new_global_weights)
 
-global_test = cluster_client_models[0][0].test_df
-yhat = meta_model.predict(global_test)
-yhat = scalar.inverse_transform(yhat)
-global_test = scalar.inverse_transform(global_test.to_numpy().reshape(-1, 1))
 
-plt.xlabel('events')
-plt.ylabel('Energy (kWh)')
-plt.title(f'Meta Model Observed Prediction {time_}')
-plt.plot(global_test, label='true')
-plt.plot(yhat, label='predicted')
-plt.legend()
-plt.savefig(f'{path.as_posix()}/meta_model_cluster_1_client_1_{int(time_)}.png')
-plt.clf()
+
+yhat, actual = plot_evaluation(cluster_client_models[0][0].x_train, cluster_client_models[0][0].x_test, f'Meta Model Observed Prediction {time_}', f'{path.as_posix()}/meta_model_cluster_1_client_1_{int(time_)}.png')
+
+# global_test_x = cluster_client_models[0][0].x_test
+# global_test_y = cluster_client_models[0][0].y_test
+# yhat = meta_model.predict(global_test_x)
+# yhat = scalar.inverse_transform(yhat)
+# global_test_y = pd.DataFrame(scalar.inverse_transform(global_test_y.reshape(-1, 1)))
+
+# plt.xlabel('events')
+# plt.ylabel('Energy (kWh)')
+# plt.title(f'Meta Model Observed Prediction {time_}')
+# plt.plot(global_test_y, label='true')
+# plt.plot(yhat, label='predicted')
+# plt.legend()
+# plt.savefig(f'{path.as_posix()}/meta_model_cluster_1_client_1_{int(time_)}.png')
+# plt.clf()
+
 
 logs = []
 model_layout = """
@@ -321,13 +393,59 @@ meta_model.compile(optimizer='adam', loss='mean_absolute_error', metrics=[keras.
 """
 
 logs.append(f'Model description: {model_layout}')
-logs.append(f'\n\t### META MODEL ###\n')
-logs.append(f'LSTM R2 score {sklearn.metrics.r2_score(global_test, yhat)}\n')
-logs.append(f'LSTM MSE score {mean_squared_error(global_test, yhat)}\n')
-logs.append(f'LSTM MAPE score {mean_absolute_percentage_error(global_test, yhat)}\n')
-logs.append(f'LSTM MAE score {mean_absolute_error(global_test, yhat)}\n')
-logs.append(f'LSTM MDAE score {median_absolute_error(global_test, yhat)}\n')
-logs.append(f'LSTM RMSE score {math.sqrt(mean_squared_error(global_test, yhat))}\n')
+logs.append(f'\n\t### META MODEL OBSERVED ###\n')
+logs.append(f'LSTM R2 score {r2_score(actual, yhat)}\n')
+logs.append(f'LSTM MSE score {mean_squared_error(actual, yhat)}\n')
+logs.append(f'LSTM MAPE score {mean_absolute_percentage_error(actual, yhat)}\n')
+logs.append(f'LSTM MAE score {mean_absolute_error(actual, yhat)}\n')
+logs.append(f'LSTM MDAE score {median_absolute_error(actual, yhat)}\n')
+logs.append(f'LSTM RMSE score {math.sqrt(mean_squared_error(actual, yhat))}\n')
+
+yhat, actual = plot_evaluation(meta_model.predict(normalized_unobserved_test_x), normalized_unobserved_test_y[0], f'Meta Model Unobserved Prediction {time_}\n TED THOMPSON #1', f'{path.as_posix()}/meta_model_unobserved_client_{int(time_)}.png')
+
+# global_test = normalized_unobserved_test_1
+# yhat = meta_model.predict(global_test)
+# yhat = scalar.inverse_transform(yhat)
+# global_test = scalar.inverse_transform(global_test.to_numpy().reshape(-1, 1))
+
+# plt.xlabel('events')
+# plt.ylabel('Energy (kWh)')
+# plt.title(f'Meta Model Unobserved Prediction {time_}\n TED THOMPSON #1')
+# plt.plot(normalized_unobserved_test_2, label='true')
+# plt.plot(yhat, label='predicted')
+# plt.legend()
+# plt.savefig(f'{path.as_posix()}/meta_model_unobserved_client_{int(time_)}.png')
+# plt.clf()
+
+logs.append(f'\n\t### META MODEL UNOBSERVED ###\n')
+logs.append(f'LSTM R2 score {r2_score(actual, yhat)}\n')
+logs.append(f'LSTM MSE score {mean_squared_error(actual, yhat)}\n')
+logs.append(f'LSTM MAPE score {mean_absolute_percentage_error(actual, yhat)}\n')
+logs.append(f'LSTM MAE score {mean_absolute_error(actual, yhat)}\n')
+logs.append(f'LSTM MDAE score {median_absolute_error(actual, yhat)}\n')
+logs.append(f'LSTM RMSE score {math.sqrt(mean_squared_error(actual, yhat))}\n')
+
+# global_test = tf_test_df_2
+# yhat = meta_model.predict(tf_test_df_2)
+# # yhat = tf_scalar.inverse_transform(yhat)
+# # global_test = tf_scalar.inverse_transform(global_test.to_numpy().reshape(-1, 1))
+
+# plt.xlabel('events')
+# plt.ylabel('Energy (kWh)')
+# plt.title(f'Meta Model Unobserved Prediction TF {time_}\n')
+# plt.plot(global_test, label='true')
+# plt.plot(pd.DataFrame(yhat, index=global_test.index), label='predicted')
+# plt.legend()
+# plt.savefig(f'{path.as_posix()}/meta_model_unobserved_tf_{int(time_)}.png')
+# plt.clf()
+
+# logs.append(f'\n\t### META MODEL UNOBSERVED ###\n')
+# logs.append(f'LSTM R2 score {sklearn.metrics.r2_score(global_test, yhat)}\n')
+# logs.append(f'LSTM MSE score {mean_squared_error(global_test, yhat)}\n')
+# logs.append(f'LSTM MAPE score {mean_absolute_percentage_error(global_test, yhat)}\n')
+# logs.append(f'LSTM MAE score {mean_absolute_error(global_test, yhat)}\n')
+# logs.append(f'LSTM MDAE score {median_absolute_error(global_test, yhat)}\n')
+# logs.append(f'LSTM RMSE score {math.sqrt(mean_squared_error(global_test, yhat))}\n')
 
 with open(f'{path.as_posix()}/meta_model_log.txt', 'w') as file:
     file.write(f'TIMESTAMP: {time_}\n')
